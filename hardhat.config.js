8aad4d2119bb031d2db8e7881d3036f4bc813025
anupamakoirala <anukoirala.96@hotmail.com>
2023-04-18 11:54:15 +0545

    configuration added

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+// require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
+
+require("@nomiclabs/hardhat-truffle5");
+require("@nomiclabs/hardhat-ethers");
+
+
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+};
